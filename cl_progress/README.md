e1f2e2cbce0d3c605f0797e7a52fb7f7899a1b21
Yoshihiro Tanaka <feria.primavera@gmail.com>
2015-02-02 12:09:37 +0900

    added cl_progress

---
+++ cl_progress/README.md
@@ -0,0 +1,15 @@
+# cl_progress
+
+## usage
+
+```bash
+% python
+>>> from cl_progress.cl_progress import *
+>>>
+>>> # True : process succeed (output [SUCCEED])
+>>> # False: process failed (output [FAILED])
+>>> printResult("process 1", True)
+process 1                                                                           [SUCCEED]
+>>> printResult("process 2", False)
+process 2                                                                            [FAILED]
+```
---
+++ cl_progress/cl_progress.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding:utf-8
+#
+# Copyright [2015] [Yoshihiro Tanaka]
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__Author__ =  "Yoshihiro Tanaka"
+__date__   =  "2015-02-02"
+
+def printResult(sent, flag):
+    import sys, commands
+
+    _SUC = '[SUCCEED]'
+    _FAL = '[FAILED]'
+
+    # ref. http://d.hatena.ne.jp/heavenshell/20090909/1252509749
+    colors = {
+        'clear': '\033[0m',
+        'red'  : '\033[31m',
+        'green': '\033[32m'
+    }
+
+    width  = int(commands.getoutput('stty size').split()[1])
+
+    if flag:
+        result = _SUC
+        color  = 'green'
+    else:
+        result = _FAL
+        color  = 'red'
+
+    spaces = width - (len(sent) + len(result))
+
+    sys.stdout.write('%s%s'   % (colors['clear'], sent + (' ' * spaces)))
+    sys.stdout.write('%s%s\n' % (colors[color], result))
